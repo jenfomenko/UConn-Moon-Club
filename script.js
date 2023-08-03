c54cd7d83249ba2cbdadf179a8b50322843ba8a7
jenfomenko <89885510+jenfomenko@users.noreply.github.com>
2023-08-02 22:23:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,31 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.131.2/build/three.m
 // Set up scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('moon-canvas') });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-// Create moon mesh
-const moonTexture = new THREE.TextureLoader().load('./nasa-sourced.jpg'); // Replace with the actual path
+// Load the moon texture
+const moonTexture = new THREE.TextureLoader().load('nasa-sourced.jpg');
 const moonGeometry = new THREE.SphereGeometry(5, 32, 32);
 const moonMaterial = new THREE.MeshBasicMaterial({ map: moonTexture });
 const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
 scene.add(moonMesh);
 
-// Set up mesh wraparound effect
-const wrapAround = new THREE.MeshBasicMaterial({
-  map: moonTexture,
-  side: THREE.BackSide // Renders the material on the back side of the moon
-});
-const wrapAroundMesh = new THREE.Mesh(moonGeometry, wrapAround);
-scene.add(wrapAroundMesh);
-
-// Set camera position
-camera.position.z = 15;
-
 // Animate moon
 const animateMoon = () => {
-  moonMesh.rotation.y += 0.005; // Rotate the front-side moon mesh
+  moonMesh.rotation.y += 0.005;
 };
+
+// Camera position
+camera.position.z = 15;
+
+// Animate the scene
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  animateMoon(); // Call the moon rotation animation
+
+  renderer.render(scene, camera);
+};
+
+animate();
