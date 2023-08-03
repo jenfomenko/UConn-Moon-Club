de3f6bbe34cef792bccb00d0cc1c81e889b5ab27
jenfomenko <89885510+jenfomenko@users.noreply.github.com>
2023-08-02 20:45:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,47 @@ const animate = () => {
 };
 
 animate();
+
+
+// Load the transparent PNG image as the particle texture
+const particleTexture = new THREE.TextureLoader().load('star.png'); // Replace with the actual path
+
+// Create particle system for stars
+const starsGeometry = new THREE.BufferGeometry();
+const starsVertices = [];
+for (let i = 0; i < 1000; i++) {
+  const x = (Math.random() - 0.5) * 200;
+  const y = (Math.random() - 0.5) * 200;
+  const z = (Math.random() - 0.5) * 200;
+  starsVertices.push(x, y, z);
+}
+starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starsVertices, 3));
+
+const starsMaterial = new THREE.PointsMaterial({
+  size: 0.1, // Adjust the size of the stars
+  map: particleTexture, // Use the transparent PNG image as the particle texture
+  transparent: true,
+  blending: THREE.AdditiveBlending,
+  depthTest: false,
+  depthWrite: false
+});
+
+const stars = new THREE.Points(starsGeometry, starsMaterial);
+stars.visible = false;
+scene.add(stars);
+
+// Detect touch or click interaction
+// ... (existing interaction detection code)
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  if (interactionDetected) {
+    stars.rotation.y += 0.01; // Rotate stars
+  }
+
+  moonMesh.rotation.y += 0.005;
+  renderer.render(scene, camera);
+};
+
+animate();
